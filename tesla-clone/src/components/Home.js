9535af134de918aa424ffe5fb477f377b7157883
regino29 <demegino@gmail.com>
2021-11-18 16:39:27 +0200

    img

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <Section
         title="Model S"
         description="Order Online for Touchless Delivery"
-        backgroundImg="model-s.jpg"
+        backgroundImg="../../public/images/model-s.jpg"
         leftBtnText="Custom order"
         rightBtnText="Existing inventory"
       />
